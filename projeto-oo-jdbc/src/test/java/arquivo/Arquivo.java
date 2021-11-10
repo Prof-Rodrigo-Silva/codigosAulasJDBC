2882f94a13008e714793efedc6a4da55594f3b48
rodrigo <prof-rodrigo-silva@users.noreply.github.com>
2021-11-10 21:23:39 -0200

    Aula Arquivo

--- projeto-oo-jdbc/src/test/java/arquivo/Arquivo.java
+++ projeto-oo-jdbc/src/test/java/arquivo/Arquivo.java
@@ -3,13 +3,17 @@ package arquivo;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
+
+import dao.ClasseDAO;
+import model.BeanAlunoFone;
 
 public class Arquivo {
 	
 	@Test
-	public void initArquivo() throws IOException {
+	public void initGravaArquivo() throws IOException {
 		File arquivo = new File("C:\\Users\\fermat\\git\\repository3\\projeto-oo-jdbc\\src\\test\\java\\arquivo\\arquivo.txt");
 		
 		if(!arquivo.exists()) {
@@ -18,7 +22,17 @@ public class Arquivo {
 		
 		FileWriter escrever_arquivo = new FileWriter(arquivo);
 		
-		escrever_arquivo.write("TEXTO");
+		//escrever_arquivo.write("TEXTO");
+		try {
+			ClasseDAO classeDao = new ClasseDAO();
+			List<BeanAlunoFone> list = classeDao.listarAlunoFone(11L);
+			
+			for(BeanAlunoFone b : list) {
+				escrever_arquivo.write(b.getNome()+";"+b.getEmail()+";"+b.getNumero()+";"+b.getTipo()+"\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
 		escrever_arquivo.flush();//Persistir
 		escrever_arquivo.close();//Fechar
 	
