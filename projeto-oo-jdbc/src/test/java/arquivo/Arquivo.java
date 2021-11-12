6b418877f0e23ea95563a44dc8c3249abc2efe2c
rodrigo <prof-rodrigo-silva@users.noreply.github.com>
2021-11-12 10:22:33 -0200

    Aula 13 - Atualização

--- projeto-oo-jdbc/src/test/java/arquivo/Arquivo.java
+++ projeto-oo-jdbc/src/test/java/arquivo/Arquivo.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,6 +20,8 @@ public class Arquivo {
 	public void initGravaArquivo() throws IOException {
 		File arquivo = new File("C:\\Users\\fermat\\git\\repository3\\projeto-oo-jdbc\\src\\test\\java\\arquivo\\arquivo.txt");
 		
+		//File arquivo = new File("C:\\Users\\fermat\\git\\repository3\\projeto-oo-jdbc\\src\\test\\java\\arquivo\\arquivo.csv");
+
 		if(!arquivo.exists()) {
 			arquivo.createNewFile();
 		}
@@ -45,14 +48,40 @@ public class Arquivo {
 		FileInputStream entrada = new FileInputStream(
 				new File("C:\\Users\\fermat\\git\\repository3\\projeto-oo-jdbc\\src\\test\\java\\arquivo\\arquivo.txt"));
 		
+		
+		//FileInputStream entrada = new FileInputStream(
+		//		new File("C:\\Users\\fermat\\git\\repository3\\projeto-oo-jdbc\\src\\test\\java\\arquivo\\arquivo.csv"));
+		
 		Scanner lerArquivo = new Scanner(entrada,"UTF-8");
 		
-		while (lerArquivo.hasNext()) {
+		//Ler arquivo
+		/*while (lerArquivo.hasNext()) {
 			String linha = lerArquivo.nextLine();
 			
 			if(linha != null && !linha.isEmpty()) {
 				System.out.println(linha);
 			}	
+		}*/
+		
+		//Ler arquivo e add em uma lista de objetos
+		List<BeanAlunoFone> list = new ArrayList<BeanAlunoFone>();
+		while (lerArquivo.hasNext()) {
+			String linha = lerArquivo.nextLine();
+			
+			if(linha != null && !linha.isEmpty()) {
+				String[] dados = linha.split("\\;");
+				
+				BeanAlunoFone beanAlunoFone = new BeanAlunoFone();
+				beanAlunoFone.setNome(dados[0]);
+				beanAlunoFone.setEmail(dados[1]);
+				beanAlunoFone.setNumero(dados[2]);
+				beanAlunoFone.setTipo(dados[3]);
+				
+				list.add(beanAlunoFone);
+			}	
+		}
+		for(BeanAlunoFone b : list) {
+			System.out.println(b.toString());
 		}
 	}
 }
